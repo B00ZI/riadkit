a13127e9f173cc6a42e3f39d08929b4a3883156c
B00ZI <iraamaneyahya@gmail.com>
2026-06-11 00:31:03 +0100

    feat: add room management routes to API; include index, store, and destroy methods

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RiadController;
+use App\Http\Controllers\RoomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,8 +20,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/settings', [RiadController::class, 'show']);
     Route::put('/settings', [RiadController::class, 'update']);
     
+
+    // Rooms
+    Route::get('/rooms', [RoomController::class, 'index']);
+    Route::post('/rooms', [RoomController::class, 'store']);
+    Route::delete('/rooms/{id}', [RoomController::class, 'destroy']);
 });
 
-Route::get('/test', function () {
-    return response()->json(['message' => 'API is working!']);
-});
